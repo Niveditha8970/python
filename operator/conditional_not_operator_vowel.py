e202e40dc280e14778e45f5433ae365e274a75b4
Niveditha8970 <117922358+Niveditha8970@users.noreply.github.com>
2022-11-14 19:44:29 +0530

    Add files via upload

---
+++ operator/conditional_not_operator_vowel.py
@@ -0,0 +1,17 @@
+'''
+write a program to check wether character entered by user is vowel or consonat.
+
+vowels=> a,e,i,o,u,A,E,I,O,U
+consonats: other than vowels are consonats.
+
+in and not in are called as membership operators.
+'''
+
+ch=input("enter charcharcters:") #i,T
+
+if ch in('a','e','i','o','u','A','E','I','O','U'):
+
+    print("it is vowel")
+
+else:
+            print("it is consonant")
---
+++ operator/not_in_conditional.py
@@ -0,0 +1,21 @@
+'''
+user enter number between 1 and 1. if user entered number as 8,9 then program must print invaild number otherwise
+it must print valid number
+'''
+'''
+n=int(input("enter a number:"))
+
+if n in(1,2,3,4,5,6,7,10):
+    print(" number is valid")
+
+else:
+    print("number is invaild")
+
+'''
+n=int(input("enter a number:"))
+
+if n not in(8,9):
+    print(" number is valid")
+
+else:
+    print("number is invaild")
